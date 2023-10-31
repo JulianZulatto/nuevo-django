814ecd67b8d9eff803f678bcc26888a02d58751d
julian zulatto <julianzulatto14@outlook.com>
2023-10-31 14:43:17 -0300

    playgroundIII

--- inicio/models.py
+++ inicio/models.py
@@ -5,3 +5,6 @@ class Paleta(models.Model):
     marca = models.CharField(max_length=30)#suele tener limite de caracteres 
     descripcion = models.TextField()#no tiene limite de caracteres
     anio = models.IntegerField()
+    
+    def __str__(self):
+        return f'{self.id} - {self.marca} - {self.anio}'
